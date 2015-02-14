b65a48690eb7e209983c82b50dfb65563a74a39d
Travis Webb <me@traviswebb.com>
2015-02-14 14:15:16 -0500

    update testing strategy

--- test/index.js
+++ test/index.js
@@ -1,6 +1,13 @@
-var _ = require('lodash');
-var najax = require('najax');
+/* global hashware */
 
+'use strict';
+
+var assert = require('assert');
+var path = require('path');
+var SailsApp = require('sails').Sails;
+var _ = require('lodash');
+
+/*
 najax.defaults({
   url: {
     hostname: 'localhost',
@@ -11,24 +18,62 @@ najax.defaults({
     Authorization: 'Basic YWRtaW46YWRtaW4xMjM0'  // encoding of default admin user/pass
   }
 });
+*/
 
-global.Backbone = require('backbone');
-global.Backbone.$ = { ajax: najax };
-require('backbone-relational');
-require('backbone-validation');
+var BackboneClient = require('../');
+global.Backbone.ajax = require('backbone.ajax');
 
 var util = require('util');
 var assert = require('assert');
-var API = require('../');
 var HASHWARE_BASEURL = 'http://localhost:1337';
 var HASHWARE_BACKBONEMODELS_URL = HASHWARE_BASEURL + '/api/v1/backbonemodel';
 
 describe('hashware-backbone-client', function () {
-  describe('#create', function () {
+  var url = 'http://localhost:1337/api/v1/backbonemodel';
+  var schema;
+  var app = new SailsApp();
+  var hashpanel;
+  var ns = {
+    Miner: {
+      foo: function () {
+        return 'bar';
+      },
+      whoami: function () {
+        return this.name;
+      }
+    }
+  };
+
+  var config = {
+    appPath: path.dirname(require.resolve('hashware-api')),
+    hooks: {
+      grunt: false
+    },
+    config: {
+      adminUsername: 'admin@hashpanel.io',
+      adminPassword: 'admin1234',
+      adminEmail: 'admin@hashpanel.io'
+    }
+  };
+
+  before(function (done) {
+    this.timeout(60 * 1000);
+
+    BackboneClient = require('./');
+    global.Backbone.ajax = require('backbone.ajax');
+
+    app.lift(config, function (error, sails) {
+      app = sails;
+
+      done(error);
+    });
+  });
+
+  describe('#create()', function () {
     this.timeout(60 * 1000);
 
     before(function (done) {
-      API.create(HASHWARE_BACKBONEMODELS_URL)
+      BackboneClient.create(HASHWARE_BACKBONEMODELS_URL)
         .then(function (api) {
           global.hashware = api;
           require('./models');
